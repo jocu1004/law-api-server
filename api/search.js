84fcd9318d38acc35eaf0b8d9d39f9a32d8a397e
Minsub Choi <jocu1004@gmail.com>
2025-07-18 13:50:15 +0900

    Fix: ESM export, package.json update for Vercel deploy

--- api/search.js
+++ api/search.js
@@ -1,22 +1,18 @@
-const axios = require('axios');
-const xml2js = require('xml2js');
+import axios from 'axios';
+import { Parser } from 'xml2js';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const { keyword } = req.query;
   const OC = 'jocu1004';
   const url = `https://www.law.go.kr/DRF/lawSearch.do?OC=${OC}&target=law&query=${encodeURIComponent(keyword)}&type=XML`;
 
   try {
     const response = await axios.get(url);
-    const parser = new xml2js.Parser({ explicitArray: false });
-    parser.parseString(response.data, (err, result) => {
-      if (err) {
-        return res.status(500).json({ error: 'XML parsing failed', detail: err.message });
-      }
-      return res.status(200).json(result);
-    });
+    const parser = new Parser({ explicitArray: false });
+    const result = await parser.parseStringPromise(response.data);
+    return res.status(200).json(result);
   } catch (error) {
     console.error('Request failed:', error.message);
     return res.status(500).json({ error: 'Request failed', detail: error.message });
   }
-};
+}
