415811069afc024ae59a9a31f8735cbfac3004a1
Minsub Choi <jocu1004@gmail.com>
2025-07-21 13:21:52 +0900

    feat: Add law history API integration

--- api/search.js
+++ api/search.js
@@ -1,28 +1,37 @@
 import axios from 'axios';
 import { Parser } from 'xml2js';
 
+import axios from 'axios';
+import { Parser } from 'xml2js';
+
 export default async function handler(req, res) {
   const { keyword } = req.query;
   const OC = 'jocu1004';
 
   const urlLaw = `https://www.law.go.kr/DRF/lawSearch.do?OC=${OC}&target=law&query=${encodeURIComponent(keyword)}&type=XML`;
   const urlPrec = `https://www.law.go.kr/DRF/lawSearch.do?OC=${OC}&target=prec&query=${encodeURIComponent(keyword)}&type=XML`;
+  const urlHistory = `https://www.law.go.kr/DRF/lawSearch.do?OC=${OC}&target=lawHistory&query=${encodeURIComponent(keyword)}&type=XML`;
 
   try {
-    const [responseLaw, responsePrec] = await Promise.all([
+    const [responseLaw, responsePrec, responseHistory] = await Promise.all([
       axios.get(urlLaw),
-      axios.get(urlPrec)
+      axios.get(urlPrec),
+      axios.get(urlHistory)
     ]);
 
     const parser = new Parser({ explicitArray: false });
 
-    const lawResult = await parser.parseStringPromise(responseLaw.data);
-    const precResult = await parser.parseStringPromise(responsePrec.data);
+    const [lawResult, precResult, historyResult] = await Promise.all([
+      parser.parseStringPromise(responseLaw.data),
+      parser.parseStringPromise(responsePrec.data),
+      parser.parseStringPromise(responseHistory.data)
+    ]);
 
     res.setHeader('Content-Type', 'application/json');
     return res.status(200).json({
       laws: lawResult,
-      precedents: precResult
+      precedents: precResult,
+      lawHistory: historyResult
     });
 
   } catch (error) {
